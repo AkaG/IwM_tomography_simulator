a9ad51b2d0a998e56a228174416ba6776c7801ec
Jakub Stanczak <kuba0095@gmail.com>
2017-03-16 00:04:05 +0100

    First try sinogram

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 from skimage import data
-from pylab import *
-import time
 
 from sinogram_generator import *
 
-def setUpPixel(x, y, img, val):
-    img[x,y] = val
 
 def main():
-
-    image = data.imread("img/Kropka.jpg", as_grey=True)
+    image = data.imread("img/Kwadraty2.jpg", as_grey=True)
     fig = plt.figure(1)
-    ax = fig.add_subplot(111)
-    # bresenham(0, 0, 200, 400, image, lambda x,y : setUpPixel(x,y,image,1.0))
-    generate_sinogram(image)
+    ax = fig.add_subplot(121)
     ax.imshow(image, cmap=cm.Greys_r, vmin=0, vmax=1)
+
+    img = generate_sinogram(image, da=1, n=70, phi=160)
+    ax = fig.add_subplot(122)
+    ax.imshow(img, cmap=cm.Greys_r, vmin=0, vmax=1)
+
     plt.show()
     return
+
 
 if __name__ == '__main__':
     main()
--- sinogram_generator.py
+++ sinogram_generator.py
@@ -17,7 +17,7 @@ def bresenham(x, y, x2, y2, img, fun_to_exec_on_points=lambda x, y: None):
         dx2 = 0
 
     numerator = length // 2
-    for i in range(0, length , 1):
+    for i in range(0, length, 1):
         fun_to_exec_on_points(x, y)
         numerator += height
         if numerator >= length:
@@ -28,19 +28,32 @@ def bresenham(x, y, x2, y2, img, fun_to_exec_on_points=lambda x, y: None):
             x += dx2
             y += dy2
 
-# da - step, n - sensors per step, l - spread
-def generate_sinogram(img, da=1, n=20, phi=30):
-    res = []
-    r = (len(img) // 2) - 1
 
-    for alpha in range(0, 180, da):
-        xe = r * cos(radians(alpha)) + r
-        ye = r * sin(radians(alpha)) + r
-        # img[int(xe), int(ye)] = 1
+def calc_weakening(x, y, img, tmp):
+    tmp[0] += img[x, y]
+    if img[x, y] > 0:
+        tmp[1] += 1
+    tmp[2] += 1
+    # img[x,y] = 0.5
+
+
+# da - step, n - sensors per step, l - spread
+def generate_sinogram(img, da=1, n=50, phi=100):
+    res = []
+    r = (len(img) // 2)
+
+    for alpha in frange(0, 180, da):
+        xe = r * cos(radians(alpha)) + r - 1
+        ye = r * sin(radians(alpha)) + r - 1
+        tmp = []
         for i in range(0, n, 1):
             deg = alpha - (phi / 2) + (i * (phi / n))
-            xd = r * cos(radians(deg) + pi) + r
-            yd = r * sin(radians(deg) + pi) + r
-            # img[int(xd), int(yd)] = 0.3
-            # print(alpha, deg, xe, ye, xd, yd)
-    return
+            xd = r * cos(radians(deg) + pi) + r - 1
+            yd = r * sin(radians(deg) + pi) + r - 1
+            bresenhamret = [0, 0, 0]
+            bresenham(int(xe), int(ye), int(xd), int(yd), img, lambda x, y: calc_weakening(x, y, img, bresenhamret))
+            tmp.append(pow(bresenhamret[0], 2))
+        res.append(tmp)
+
+    res = divide(res, amax(res))
+    return res
